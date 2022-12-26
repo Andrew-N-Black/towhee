5c23e6ca6a056b79e9af391b6b8846d170d819ff
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-12-26 09:39:17 -0800

    Update heterozygosity.R

--- R/heterozygosity.R
+++ R/heterozygosity.R
@@ -9,3 +9,7 @@ ggsave("~/towhee.svg")
 ggplot(HET,aes(x=reorder(Sites,HET),y=HET,fill=Pop))+geom_boxplot(show.legend =FALSE)+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+theme_classic()+scale_fill_manual(values=c("grey","bisque","black","cadetblue"))+theme(axis.text.x=element_text(angle = 45, vjust = 0.75))
 
 
+#Summarize by Population
+library(dplyr)
+ind_roh$Pop <- ordered(ind_roh$Pop,levels = c("OREG","CCAL","INYO","SCAL"))
+group_by(het, Pop) %>% summarise(count = n(),mean = mean(`HET`, na.rm = TRUE),sd = sd(`HET`, na.rm = TRUE),median = median(`HET`, na.rm = TRUE),IQR = IQR(`HET`, na.rm = TRUE))
