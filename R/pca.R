2c9be3c0429459abd0999500d262086fd01f476c
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-02-23 20:06:05 -0800

    Update pca.R

--- R/pca.R
+++ R/pca.R
@@ -11,11 +11,10 @@ sample_pop_sites <- read_excel("sample-pop-sites.xlsx")
 cov<-as.matrix(read.table("Figure 3.cov"))
 head(axes$values/sum(axes$values)*100)
 #[1] 7.633992 5.665231 4.160406 2.030399 1.893622
-PC1_3<-as.data.frame(axes$vectors[,1:3])
-title1<-"Region"
-title2<-"site"
-x<-cbind(PC1_3,sample_pop_sites)
-ggplot(data=x,aes(y=V2, x=V1, color=Pop))+geom_point(size=5,aes(fill=Pop),colour="black",pch=21)+ theme_classic() + xlab("PC1 (7.63%)") +ylab("PC2 (5.66%)")+scale_fill_manual(title1, values =c("INYO"="black","CCAL"="bisque","OREG"="azure4","SCAL"="cadetblue"))
-ggsave("~/figure_3.svg")
+ pca1<-glPca(dace,nf=2,parallel = TRUE,n.cores = 4)
+ meta<-read.table(" /Users/andrew/Library/CloudStorage/Box-Box/Foskett\ Dace\ Population\ Genetics/Files_popgen/info.txt")
+ DACE<-cbind(pca1$scores,meta)
+ ggplot(data=DACE,aes(y=PC2, x=PC1,group=V2,shape=V2))+geom_point(size=5,aes(fill=V2,shape=V2),colour="black",pch=21)+ theme_classic() + xlab("PC1 (15.5%)") +ylab("PC2 (6.0%)")+scale_fill_manual("Population", values =c("Coleman_Creek"="grey20","Dace_Spring"="grey","Deep_Creek"="grey40","Foskett_Spring"="grey92","Twentymile_Creek"="white"))
+ ggsave("~/figure_3.svg")
 
 
