70f057bd5cb6b12d49c0c5d5f9d8e8b9b2f201b0
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-03-20 20:03:54 -0700

    Create pca.sh

---
+++ pca.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+#SBATCH -A fnrpupfish
+#SBATCH -n 64
+#SBATCH -t 10-00:00:00
+#SBATCH --job-name=pca
+#SBATCH --error=pca.e
+#SBATCH --output=pca.o
+#SBATCH --mem=64G
+module load biocontainers
+module load angsd
+module load pcangsd
+#Move to bam folder
+cd /scratch/bell/blackan/TOWHEE/angsd_out
+mkdir PCA
+angsd -GL 1 -out PCA/towhee -minQ 30 -P 64 \
+-minInd 64 -doGlf 2 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -bam bamlist \
+-ref /scratch/bell/blackan/TOWHEE/ref/NCBI/ref.fa
+
+pcangsd -b  PCA/towhee.beagle.gz --selection --minMaf 0.05 --sites_save --tree -o PCA/pca-towhee --threads 64 --pcadapt --admix
