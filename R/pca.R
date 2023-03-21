5cd3a8406025fe17c47136c8945a12344e7e19fb
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-03-21 09:09:09 -0700

    Update pca.R

--- R/pca.R
+++ R/pca.R
@@ -8,14 +8,19 @@ library(pophelper)
 #FIGURE 3
 #Figure.3.pop <- read.csv("~/Figure 3 pop.txt", sep="")
 sample_pop_sites <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/het.xlsx")
+sample_pop_sites$Pop <- factor(sample_pop_sites$Pop, levels = c("OREG","CCAL", "INYO","SCAL"))
 cov<-as.matrix(read.table("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/pca-towhee.cov"))
 axes<-eigen(cov)
 head(axes$values/sum(axes$values)*100)
 #[1] 7.751829 5.941751 4.167590 2.001211
- pca1<-glPca(cov,nf=2,parallel = TRUE,n.cores = 4)
- meta<-read.table(" /Users/andrew/Library/CloudStorage/Box-Box/Foskett\ Dace\ Population\ Genetics/Files_popgen/info.txt")
- DACE<-cbind(pca1$scores,meta)
- ggplot(data=DACE,aes(y=PC2, x=PC1,group=V2,shape=V2))+geom_point(size=5,aes(fill=V2,shape=V2),colour="black",pch=21)+ theme_classic() + xlab("PC1 (15.5%)") +ylab("PC2 (6.0%)")+scale_fill_manual("Population", values =c("Coleman_Creek"="grey20","Dace_Spring"="grey","Deep_Creek"="grey40","Foskett_Spring"="grey92","Twentymile_Creek"="white"))
- ggsave("~/figure_3.svg")
 
+PC1_3<-as.data.frame(axes$vectors[,1:3])
+title1<-"Population"
+title2<-"Site"
+
+#By Population
+ggplot(data=PC1_3, aes(y=V2, x=V1))+geom_point(size=7,pch=21,aes(fill=sample_pop_sites$Pop))+ theme_classic() + xlab("PC1 (7.75%)") +ylab("PC2 (5.94%)")+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+scale_fill_manual("Population", values=c("grey","tan2","black","cadetblue"))
+
+ggsave("~/pca.towhee.svg")
+ggsave("~/pca.towhee.pdf")
 
