6255865494da87ab4c765efa3b4681f57f3e10be
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-03-21 08:48:36 -0700

    Update pca.R

--- R/pca.R
+++ R/pca.R
@@ -8,11 +8,11 @@ library(pophelper)
 #FIGURE 3
 #Figure.3.pop <- read.csv("~/Figure 3 pop.txt", sep="")
 sample_pop_sites <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/het.xlsx")
-cov <- as.matrix("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/pca-towhee.cov")
+cov<-as.matrix(read.table("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/pca-towhee.cov"))
 axes<-eigen(cov)
 head(axes$values/sum(axes$values)*100)
-#[1] 7.633992 5.665231 4.160406 2.030399 1.893622
- pca1<-glPca(dace,nf=2,parallel = TRUE,n.cores = 4)
+#[1] 7.751829 5.941751 4.167590 2.001211
+ pca1<-glPca(cov,nf=2,parallel = TRUE,n.cores = 4)
  meta<-read.table(" /Users/andrew/Library/CloudStorage/Box-Box/Foskett\ Dace\ Population\ Genetics/Files_popgen/info.txt")
  DACE<-cbind(pca1$scores,meta)
  ggplot(data=DACE,aes(y=PC2, x=PC1,group=V2,shape=V2))+geom_point(size=5,aes(fill=V2,shape=V2),colour="black",pch=21)+ theme_classic() + xlab("PC1 (15.5%)") +ylab("PC2 (6.0%)")+scale_fill_manual("Population", values =c("Coleman_Creek"="grey20","Dace_Spring"="grey","Deep_Creek"="grey40","Foskett_Spring"="grey92","Twentymile_Creek"="white"))
