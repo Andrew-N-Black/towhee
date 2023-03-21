f604762b01d1c7298c0e233d6bf989cca0e363c0
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-03-21 07:36:07 -0700

    Update heterozygosity.R

--- R/heterozygosity.R
+++ R/heterozygosity.R
@@ -1,15 +1,25 @@
 library(ggplot2)
-HET<- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Figures/het.xlsx")
-HET$Pop <- factor(HET$Pop, levels = c("OREG","CCAL", "INYO","SCAL"))
+het <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/het.xlsx")
+het$Pop <- factor(het$Pop, levels = c("OREG","CCAL", "INYO","SCAL"))
 
 #Color by region
-ggplot(HET,aes(x=Pop,y=HET,fill=Pop))+geom_boxplot(show.legend =FALSE)+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+theme_classic()+scale_fill_manual(values=c("grey","bisque","black","cadetblue"))+theme(legend.position="none")
-ggsave("~/towhee.svg")
+ggplot(het,aes(x=Pop,y=HET,fill=Pop))+geom_boxplot(show.legend =FALSE)+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+theme_classic()+scale_fill_manual(values=c("grey","bisque","black","cadetblue"))+theme(legend.position="none")
+ggsave("~/het_towhee.svg")
 #Color by region and order by site
-ggplot(HET,aes(x=reorder(Sites,HET),y=HET,fill=Pop))+geom_boxplot(show.legend =FALSE)+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+theme_classic()+scale_fill_manual(values=c("grey","bisque","black","cadetblue"))+theme(axis.text.x=element_text(angle = 45, vjust = 0.75))
-
+ggplot(het,aes(x=reorder(Sites,HET),y=HET,fill=Pop))+geom_boxplot(show.legend =TRUE)+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+theme_classic()+scale_fill_manual("Population",values=c("grey","bisque","black","cadetblue"))+theme(axis.text.x=element_text(angle = 45, vjust = 0.8,hjust = 1))
+ggsave("~/het_site_towhee.svg")
 
 #Summarize by Population
 library(dplyr)
-ind_roh$Pop <- ordered(ind_roh$Pop,levels = c("OREG","CCAL","INYO","SCAL"))
+
 group_by(het, Pop) %>% summarise(count = n(),mean = mean(`HET`, na.rm = TRUE),sd = sd(`HET`, na.rm = TRUE),median = median(`HET`, na.rm = TRUE),IQR = IQR(`HET`, na.rm = TRUE))
+
+ Pop   count    mean        sd  median
+  <fct> <int>   <dbl>     <dbl>   <dbl>
+1 OREG     14 0.00169 0.0000630 0.00171
+2 CCAL     30 0.00201 0.000127  0.00203
+3 INYO     14 0.00180 0.000188  0.00185
+4 SCAL     23 0.00246 0.0000572 0.00247
+
+#ind_roh$Pop <- ordered(ind_roh$Pop,levels = c("OREG","CCAL","INYO","SCAL"))
+#group_by(het, Pop) %>% summarise(count = n(),mean = mean(`HET`, na.rm = TRUE),sd = sd(`HET`, na.rm = TRUE),median = median(`HET`, na.rm = TRUE),IQR = IQR(`HET`, na.rm = TRUE))
