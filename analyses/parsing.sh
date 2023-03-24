97c08ab64f0e9c6160f78973cdd7d06ec823580b
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-03-23 18:26:16 -0700

    Update parsing.sh

--- analyses/parsing.sh
+++ analyses/parsing.sh
@@ -3,8 +3,7 @@ ml bedtools
 ml bioawk
 
 #GENES
-awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4-1,$5}' GCF_028551555.1_PUWL_Pcris_2_genomic.gtf |  bedtools sort | bedtools merge -i - > towhee_genes.bed
-
+cat GCF_028551555.1_PUWL_Pcris_2_genomic.gtf | awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4,$5}' > towhee_genes.bed
 
 #INTER
 awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4,$5}' GCF_028551555.1_PUWL_Pcris_2_genomic.gtf > exon.bed
