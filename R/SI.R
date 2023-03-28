9dda4bf6286c263453088d793558a01f626321ed
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-03-28 12:02:00 -0700

    Update SI.R

--- R/SI.R
+++ R/SI.R
@@ -4,13 +4,16 @@ library(readxl)
 
 #Subet length of scaffolds and plot
 length_scaff <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/Sequence_lengths_reference.xlsx")
+new<-subset(length_scaff,LengthKB < 100,select=c(Contig,LengthKB))
 ggplot(data=length_scaff, aes(y=LengthKB,x=reorder(LengthKB,Contig)))+geom_bar(stat="identity")+theme_classic()+ylab("Length (kb)")+xlab("Contig")+theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())+ geom_hline(yintercept=100, linetype="dashed", color = "red")
 
 
 ggsave("S1a.svg")
 ggsave("S1a.pdf")
 
-
+ggplot(data=new, aes(y=LengthKB,x=reorder(LengthKB,Contig)))+geom_bar(stat="identity")+theme_classic()+ylab("Length (kb)")+xlab("Contig")+theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())+ geom_hline(yintercept=100, linetype="dashed", color = "red")
+ggsave("S1aa.svg")
+ggsave("S1aa.pdf")
 
 
 #Pre/post breadth
@@ -18,6 +21,14 @@ depth_breadth <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdu
 depth_breadth$Pop <- factor(depth_breadth$Pop, levels = c("OREG","CCAL", "INYO","SCAL"))
 
 ggplot(data=depth_breadth, aes(y=Breadth5x, x=reorder(SampleID,Breadth5x),fill=Pop))+geom_bar(stat="identity")+theme_classic()+scale_fill_manual("Population",values=c("darkorchid","tan2","black","cadetblue"))+xlab("Sample (N=81)")+ylab("5x Breadth")+theme( axis.ticks.x=element_blank(),axis.text.x = element_blank())+facet_wrap(~Filter,ncol=1)
+
+
+
+#Pre/post Depth
+depth_breadth <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/summary_stats.xlsx")
+depth_breadth$Pop <- factor(depth_breadth$Pop, levels = c("OREG","CCAL", "INYO","SCAL"))
+
+ggplot(data=depth_breadth, aes(y=Depth, x=reorder(SampleID,Depth),fill=Pop))+geom_bar(stat="identity")+theme_classic()+scale_fill_manual("Population",values=c("darkorchid","tan2","black","cadetblue"))+xlab("Sample (N=81)")+ylab("Depth")+theme( axis.ticks.x=element_blank(),axis.text.x = element_blank())+facet_wrap(~Filter,ncol=1)
 
 
 #Mean Depth
