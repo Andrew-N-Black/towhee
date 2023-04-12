ec8a6edf38f53bab210d1225d6e7bad35c5460b7
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-04-11 19:23:10 -0700

    Update ind_ROH.R

--- R/ind_ROH.R
+++ R/ind_ROH.R
@@ -4,10 +4,10 @@ library(reshape2)
 
 ind_roh <-read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/ind_rohs.xlsx")
 ind_roh<-as.data.frame(ind_roh)
-melt_data<-melt(ind_roh,id.vars = c("sample","Pop","Sites"))
+melt_data<-melt(ind_roh,id.vars = c("ID","Pop","Sites"))
 melt_data$Pop <- factor(melt_data$Pop, levels = c("OREG","CCAL", "INYO","SCAL"))
 
-ggplot(melt_data, aes(fill=Pop, y=value, x=reorder(sample,value))) +geom_bar(stat="identity",position="dodge")+facet_grid(variable~Pop,scales = "free")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+theme_classic()+
+ggplot(melt_data, aes(fill=Pop, y=value, x=reorder(ID,value))) +geom_bar(stat="identity",position="dodge")+facet_grid(variable~Pop,scales = "free")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+theme_classic()+
     scale_fill_manual(values=c("darkorchid","tan2","black","cadetblue"))+ylab("fROH")+xlab("Sample (N=81)")+
     theme(legend.position="none")+theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
     
@@ -92,21 +92,22 @@ ind_roh$Pop <- ordered(ind_roh$Pop,levels = c("OREG","CCAL","INYO","SCAL"))
 
 group_by(ind_roh, Pop) %>% summarise(count = n(),mean = mean(`100kb-1MB`, na.rm = TRUE),sd = sd(`100kb-1MB`, na.rm = TRUE),median = median(`100kb-1MB`, na.rm = TRUE),IQR = IQR(`100kb-1MB`, na.rm = TRUE))
 
-Pop   count   mean     sd median
-  <ord> <int>  <dbl>  <dbl>  <dbl>
-1 OREG     14 0.113  0.0184 0.111 
-2 CCAL     30 0.0558 0.0286 0.0626
-3 INYO     14 0.136  0.0647 0.107 
-4 SCAL     23 0.0363 0.0236 0.0217
+# A tibble: 4 × 6
+  Pop   count   mean     sd median    IQR
+  <ord> <int>  <dbl>  <dbl>  <dbl>  <dbl>
+1 OREG     14 0.112  0.0182 0.110  0.0249
+2 CCAL     30 0.0552 0.0284 0.0621 0.0447
+3 INYO     14 0.135  0.0642 0.106  0.0457
+4 SCAL     23 0.0360 0.0234 0.0216 0.0445
 
 group_by(ind_roh, Pop) %>% summarise(count = n(),mean = mean(`>1MB`, na.rm = TRUE),sd = sd(`>1MB`, na.rm = TRUE),median = median(`>1MB`, na.rm = TRUE),IQR = IQR(`>1MB`, na.rm = TRUE))
-
- Pop   count   mean      sd  median
-  <ord> <int>  <dbl>   <dbl>   <dbl>
-1 OREG     14 0.0151 0.00683 0.0136 
-2 CCAL     30 0.0192 0.00981 0.0179 
-3 INYO     14 0.0480 0.0351  0.0357 
-4 SCAL     23 0.0134 0.0104  0.00748
+ 
+Pop   count   mean      sd  median     IQR
+  <ord> <int>  <dbl>   <dbl>   <dbl>   <dbl>
+1 OREG     14 0.0150 0.00676 0.0135  0.00325
+2 CCAL     30 0.0190 0.00972 0.0177  0.0191 
+3 INYO     14 0.0476 0.0348  0.0353  0.0275 
+4 SCAL     23 0.0133 0.0103  0.00741 0.0195 
 
 
 group_by(ind_roh, Pop) %>% summarise(count = n(),mean = mean(`Total`, na.rm = TRUE),sd = sd(`Total`, na.rm = TRUE),median = median(`Total`, na.rm = TRUE),IQR = IQR(`Total`, na.rm = TRUE))
