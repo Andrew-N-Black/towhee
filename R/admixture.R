3db37a2b86d33f04a7f1a037ba473bd2d2e6f0de
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-01-03 17:15:53 -0800

    Update admixture.R

--- R/admixture.R
+++ R/admixture.R
@@ -6,14 +6,18 @@ library(pophelper)
 
 setwd("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/towhee_admix/")
 
-labels <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/labels.xlsx")
-
+labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/labels.xlsx")
 #sample_pop_sites <- read_excel("sample-pop-sites.xlsx")
 #both <- as.data.frame(sample_pop_sites[,c(2,3)])
 #bothT <- as.data.frame(sample_pop_sites[,c(3,2)])
 
 slist<-readQ(files =c("pop_K3-combined-merged.Q","pop_K4-combined-merged.Q","pop_K5-combined-merged.Q"))
-plotQ(slist[1:3],returnplot=T,exportplot=T,imgoutput = "join",clustercol=c("cadetblue","tan2","darkorchid","black","blue"),grplab=labels,ordergrp=T,showlegend=F,height=.6,indlabsize=1.8,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.1,grplabsize=0.6,barbordersize=0,linesize=0.4,showsp = F,splabsize = 0,outputfilename="plotq",imgtype="pdf",exportpath=getwd(),splab = c("K=3","K=4","K=5"),divcol = "black",splabcol="black",grplabheight=1.5,grplabangle =45)
+
+#palette.colors(8,"Paired")
+#[1] "#A6CEE3" "#1F78B4" "#B2DF8A"
+#[4] "#33A02C" "#FB9A99" "#E31A1C"
+#[7] "#FDBF6F" "#FF7F00"
+plotQ(slist[1:3],returnplot=T,exportplot=T,imgoutput = "join",clustercol=c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99"),grplab=labels,ordergrp=T,showlegend=F,height=.6,indlabsize=1.8,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.1,grplabsize=0.6,barbordersize=0,linesize=0.4,showsp = F,splabsize = 0,outputfilename="plotq",imgtype="pdf",exportpath=getwd(),splab = c("K=3","K=4","K=5"),divcol = "black",splabcol="black",grplabheight=1.5,grplabangle =45)
 
 
 
