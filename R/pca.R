6457f52a5cd19bbac239c0a1ba20048a0b8b5e3c
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-01-03 17:09:19 -0800

    Update pca.R

--- R/pca.R
+++ R/pca.R
@@ -4,6 +4,12 @@ library(ggplot2)
 library(pophelper)
 
 
+library(reshape2)
+library(readxl)
+library(ggplot2)
+library(pophelper)
+
+
 
 #FIGURE 3
 #Figure.3.pop <- read.csv("~/Figure 3 pop.txt", sep="")
@@ -19,7 +25,7 @@ title1<-"Population"
 title2<-"Site"
 
 #By Population
-ggplot(data=PC1_3, aes(y=V2, x=V1))+geom_point(size=7,pch=21,aes(fill=sample_pop_sites$Pop))+ theme_classic() + xlab("PC1 (7.75%)") +ylab("PC2 (5.94%)")+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+scale_fill_manual("Population", values=c("darkorchid","tan2","black","cadetblue"))
+ggplot(data=PC1_3, aes(y=V2, x=V1))+geom_point(size=7,pch=21,aes(fill=sample_pop_sites$Pop))+ theme_classic() + xlab("PC1 (7.75%)") +ylab("PC2 (5.94%)")+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+ scale_fill_brewer(palette="Paired")+theme(legend.title=element_blank())
 
 ggsave("~/pca.towhee.svg")
 ggsave("~/pca.towhee.pdf")
