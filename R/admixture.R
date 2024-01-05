cc4c504bccbf2d3034b18c360215a1693f4d5ff2
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-01-05 10:11:00 -0800

    Update admixture.R

--- R/admixture.R
+++ R/admixture.R
@@ -20,5 +20,8 @@ slist<-readQ(files =c("pop_K3-combined-merged.Q","pop_K4-combined-merged.Q","pop
 plotQ(slist[1:3],returnplot=T,exportplot=T,imgoutput = "join",clustercol=c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99"),grplab=labels,ordergrp=T,showlegend=F,height=.6,indlabsize=1.8,indlabheight=0.08,indlabspacer=1,barbordercolour="black",divsize = 0.1,grplabsize=0.6,barbordersize=0,linesize=0.4,showsp = F,splabsize = 0,outputfilename="plotq",imgtype="pdf",exportpath=getwd(),splab = c("K=3","K=4","K=5"),divcol = "black",splabcol="black",grplabheight=1.5,grplabangle =45)
 
 
+#MAF 0.10
+setwd("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/")
+slist<-readQ(files =c("pop_K3-combined-mergedMAF.10.txt","pop_K4-combined-mergedMAF.10.txt","pop_K5-combined-mergedMAF.10.txt"))
 
 
