562db49e03742d1d918276631af2404f9896b828
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-01-04 16:02:15 -0800

    Update admixture_pca.sh

--- R/admixture_pca.sh
+++ R/admixture_pca.sh
@@ -1,5 +1,5 @@
 library(pophelper)
-labels <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/labels.xlsx")
+labels <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/labels.xlsx")
 
-list<-readQ(files ="~/pca-towhee.admix.4.Q")
+list<-readQ(files ="/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/pca-towhee.admix.4.Q")
 plotQ(slist,returnplot=T,exportplot=T,clustercol=c("cadetblue","tan2","darkorchid","black"),grplab=labels,ordergrp=T,showlegend=F,height=3,indlabsize=1,indlabheight=0,indlabspacer=0,barbordercolour="black",divsize = 0.1,grplabsize=1.3,barbordersize=0,linesize=0.4,showsp = F,splabsize = .1,outputfilename="plotq",imgtype="png",exportpath=getwd(),splab = "K=4",divcol = "black",splabcol="black",grplabheight=0.1,grplabjust = 0.5, grplabangle = 45)
