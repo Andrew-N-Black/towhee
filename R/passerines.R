24be9b832f71e9415fa93e4a70f7ad09abb1906a
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-09-18 10:52:04 -0700

    Update passerines.R

--- R/passerines.R
+++ R/passerines.R
@@ -42,3 +42,21 @@ ggplot(passerine_roh_long_IUCN,aes(x=reorder(group,long),y=long))+geom_boxplot()
 +     axis.title.y = element_text(size = 20))
 
 
+#Summary SD
+http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization
+
+
+data_summary <- function(data, varname, groupnames){
+    require(plyr)
+    summary_func <- function(x, col){
+        c(mean = mean(x[[col]], na.rm=TRUE),
+          sd = sd(x[[col]], na.rm=TRUE))
+    }
+    data_sum<-ddply(data, groupnames, .fun=summary_func,
+                    varname)
+    data_sum <- rename(data_sum, c("mean" = varname))
+    return(data_sum)
+}
+
+df2 <- data_summary(heterozygosity_passeriformes_USFWS_mean, varname="H",groupnames="species")
+
