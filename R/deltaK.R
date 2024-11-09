3268b8e8f23032d36be6e05bf47199a3716a1cf6
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-11-09 10:40:36 -0800

    Update deltaK.R

--- R/deltaK.R
+++ R/deltaK.R
@@ -1,5 +1,5 @@
 library(ggplot2)
 library(readxl)
-delta <- read_excel("/Users/andrew/Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/Towhee/Black_analysis/k.xlsx")
-ggplot(delta,aes(x=K,y=`mean(|L"(K)|)/stdev[L(K)]")`))+geom_line(color="black") + geom_point(color="cadetblue",size=6)+theme_classic()+ylab("∆K")
-ggsave("~/delta.svg")
+delta <- read_excel("/Users/andrewblack/Documents/Research/Towhee/Black_analysis/k.xlsx")
+ggplot(delta,aes(x=K,y=`mean(|L"(K)|)/stdev[L(K)]")`))+geom_line(color="black") + geom_point(color="grey22",size=6)+theme_classic()+ylab("∆K")
+ggsave("~/delta.svg",width = 12,height = 3)
