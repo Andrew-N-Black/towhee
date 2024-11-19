c9a98d69420fc290b98843afc89fb416a9b2c9ad
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-11-18 16:21:13 -0800

    Update NeEst.R

--- R/NeEst.R
+++ R/NeEst.R
@@ -1,4 +1,5 @@
 library(ggplot2)
 library(forcats)
 k<-ggplot(Ne,aes(x=forcats::fct_reorder(Region, NE, .fun = median),y=NE))
-k+geom_point(aes(group=Region,color=Region))+geom_errorbar(aes(ymin=min,ymax=max,color=Region,width=0.2))+theme_classic()
+k+geom_point(aes(group=Region,color=Region),size=5)+geom_errorbar(aes(ymin=min,ymax=max,color=Region,width=0.6))+theme_classic()+scale_color_manual(values=c("#1F78B4","#B2DF8A","#A6CEE3","#33A02C"))+xlab("")+ theme(legend.position="none")
+
