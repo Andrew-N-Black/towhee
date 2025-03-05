ced632c5c2bfb35c733fd217d5d641a37d3ca952
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-03-05 13:43:11 -0800

    Update gone.R

--- R/gone.R
+++ R/gone.R
@@ -1,5 +1,6 @@
 ##Plot derived from:https://github.com/martykardos/KillerWhaleInbreeding/blob/main/FigureCode/rCode_Fig_ED_1.R
-#Focus on the first 50 generations:
+#Focus on the first 100 generations:
++scale_color_manual(values=c("#1F78B4","#B2DF8A","#A6CEE3","#33A02C")
 
 par(xpd=FALSE,mfrow=c(1,1))
 library(scales)
@@ -43,9 +44,9 @@ for(i in 1:500){
     NeCI[i,] <- quantile(NeMat[i,],probs=c(0.025,0.975))
     NeCI2[i,] <- quantile(NeMat[i,],probs=c(0.25,0.75))
 }
-lines(1:500,rowMedians(NeMat[1:500,]),col="#33A02C",lwd=4)
-polygon(x=c(1:500,rev(1:500)),y=c(NeCI[1:500,1],rev(NeCI[1:500,2])),col=adjustcolor("#33A02C",alpha.f=0.2),border=NA)
-polygon(x=c(1:500,rev(1:500)),y=c(NeCI2[1:500,1],rev(NeCI2[1:500,2])),col=adjustcolor("#33A02C",alpha.f=0.3),border=NA)
+lines(1:500,rowMedians(NeMat[1:500,]),col="#1F78B4",lwd=4)
+polygon(x=c(1:500,rev(1:500)),y=c(NeCI[1:500,1],rev(NeCI[1:500,2])),col=adjustcolor("#1F78B4",alpha.f=0.2),border=NA)
+polygon(x=c(1:500,rev(1:500)),y=c(NeCI2[1:500,1],rev(NeCI2[1:500,2])),col=adjustcolor("#1F78B4",alpha.f=0.3),border=NA)
 par(xpd=FALSE,mfrow=c(1,1))
 
 
@@ -72,6 +73,28 @@ for(i in 1:500){
 lines(1:500,rowMedians(NeMat[1:500,]),col="#B2DF8A",lwd=4)
 polygon(x=c(1:500,rev(1:500)),y=c(NeCI[1:500,1],rev(NeCI[1:500,2])),col=adjustcolor("#B2DF8A",alpha.f=0.2),border=NA)
 polygon(x=c(1:500,rev(1:500)),y=c(NeCI2[1:500,1],rev(NeCI2[1:500,2])),col=adjustcolor("#B2DF8A",alpha.f=0.3),border=NA)
+
+#OREG
+plot(c(0,100),c(0,3000),type="n",xlab="Generations back in time",ylab=expression(paste("Historical ",italic(""*N*"")[e],sep="")),
+     cex.lab=1.5)
+files <- paste("OREG_outfileLD_TEMP/outfileLD_",1:500,"_GONE_Nebest",sep="")
+NeMat <- NULL
+for(i in 1:500){
+    dat <- read.table(files[i],skip=2)
+    NeMat <- cbind(NeMat,dat[,2])
+}
+# get CI
+NeCI <- matrix(NA,nrow=500,ncol=2)
+NeCI2 <- matrix(NA,nrow=500,ncol=2)
+for(i in 1:500){
+    NeCI[i,] <- quantile(NeMat[i,],probs=c(0.025,0.975))
+    NeCI2[i,] <- quantile(NeMat[i,],probs=c(0.25,0.75))
+}
+lines(1:500,rowMedians(NeMat[1:500,]),col="#A6CEE3",lwd=4)
+polygon(x=c(1:500,rev(1:500)),y=c(NeCI[1:500,1],rev(NeCI[1:500,2])),col=adjustcolor("#A6CEE3",alpha.f=0.2),border=NA)
+polygon(x=c(1:500,rev(1:500)),y=c(NeCI2[1:500,1],rev(NeCI2[1:500,2])),col=adjustcolor("#A6CEE3",alpha.f=0.3),border=NA)
+par(xpd=FALSE,mfrow=c(1,1))
+                    
 
 
 
