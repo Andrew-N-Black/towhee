a4fe673eae28b91c8c0c88c3fc0de036c773aaa7
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-08-12 17:58:51 -0700

    Update gone.sh

--- analyses/gone.sh
+++ analyses/gone.sh
@@ -1,5 +1,12 @@
-
-
+#!/bin/sh -l
+#SBATCH -A fnrchook
+#SBATCH -N 1
+#SBATCH -n 128
+#SBATCH -t 14-00:00:00
+#SBATCH --job-name=gone
+#SBATCH --error=%x_%j.out
+#SBATCH --output=%x_%j.out
+#SBATCH --mem=249G
 
 POP=CCAL,SCAL,OREG,INYO
 bash script_GONE.sh POP
